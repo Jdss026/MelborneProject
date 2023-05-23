6ea2485613af1e73d2c9c3c5f9bacf58cef1ff9a
Jdss026 <jdss026@gmail.com>
2023-05-22 21:40:00 -0300

    add botao no webapp

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import sklearn
 import pickle
+import sklearn
 import pandas as pd
 
 def calculate_output(input_data):
@@ -15,6 +15,9 @@ def main():
     # Criar as 9 entradas
     input_values = []
 
+    # Importar scaler e model
+    scaler = pickle.load(open('./model/scalar.pkl', 'rb'))
+    pickled_model = pickle.load(open('./model/model.pkl', 'rb'))
 
     labels = ['Rooms',
         'Distance',
@@ -26,28 +29,18 @@ def main():
         'Propertycount', 
         'Month']
     
-
     for i in range(9):
         input_values.append(st.number_input( labels[i] , value=0.0))
 
-    df = pd.DataFrame(input_values)
-
-    # Importar scaler e model
-    scaler = pickle.load(open('./model/scalar.pkl', 'rb'))
-    pickled_model = pickle.load(open('./model/model.pkl', 'rb'))
-
-    # reescalar os valores de input
-    scaled_cols = scaler.fit_transform(input_values)
-    scaled_cols = pd.DataFrame(scaled_cols, columns=labels)
-    scaled_cols.head()
-
-    # Calcular a saída com base nas entradas
-    output = pickled_model.predict(scaled_cols)
-
-    # Exibir a saída
-    st.header("Saída:")
-    st.write(scaled_cols.head())
-    st.write(output)
+    if st.button('Predição'):
+        df = pd.DataFrame([input_values], columns=labels)
+        # reescalar os valores de input
+        scaled_cols = scaler.fit_transform(df)
+        # Calcular a saída com base nas entradas
+        output = pickled_model.predict(scaled_cols)
+        # Exibir a saída
+        st.header("Saída:")
+        st.write(output)
 
 if __name__ == "__main__":
     main()
