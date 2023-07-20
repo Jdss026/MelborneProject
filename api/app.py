bbe92a6c9fb86a8406766169fd6715996cb540c5
Jdss026 <jdss026@gmail.com>
2023-07-20 12:01:52 -0300

    add html page

---
+++ api/app.py
@@ -0,0 +1,96 @@
+from flask import Flask, jsonify, request, make_response, render_template
+import pickle
+import pandas as pd
+import numpy as np
+
+# cria o app flask
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
+#importa modelo e scaler como objetos
+model = pickle.load(open('./model/model.pkl', 'rb'))
+scaler = pickle.load(open('./model/std_scalar.pkl','rb'))
+
+#labels para colunas do df
+labels = ['Rooms',
+        'Distance',
+        'Bathroom',
+        'Car',
+        'Landsize',
+        'Lattitude',
+        'Longtitude',
+        'Propertycount', 
+        'Month']
+
+@app.route('/',methods=['GET'])
+def main():
+    return render_template('./index.html')
+
+# decorator do flask para rotear caminho a função
+@app.route('/predict_json',methods=['POST'])
+def predict():
+    features = request.get_json()
+
+    for key in labels:
+        if key not in features:
+            return jsonify({'error': f'A chave "{key}" está faltando no JSON enviado.'})
+
+    df_scalar = []
+
+    for cols in features.keys():
+        z_prod = (features[cols]-scaler[cols][0])/scaler[cols][1]
+        df_scalar.append(z_prod)
+
+    df_scalar = pd.DataFrame([df_scalar], columns=labels)
+    prediction = model.predict(df_scalar)
+
+    return make_response(
+        jsonify(mensagem = "Previsao feita com sucesso",
+                dado = prediction[0]))
+
+
+@app.route('/predict', methods=['POST'])
+def predict_index():
+    rooms         = float(request.form.get('Rooms'))
+    distance      = float(request.form.get('Distance'))
+    bathroom      = float(request.form.get('Bathroom'))
+    car           = float(request.form.get('Car'))
+    landsize      = float(request.form.get('Landsize'))
+    lattitude     = float(request.form.get('Lattitude'))
+    longtitude    = float(request.form.get('Longtitude'))
+    propertycount = float(request.form.get('Propertycount'))
+    month         = float(request.form.get('Month'))
+
+    features = pd.DataFrame(np.array([rooms,
+        distance,
+        bathroom,
+        car,
+        landsize,
+        lattitude,
+        longtitude,
+        propertycount, 
+        month]).reshape(1,9), columns=labels)
+    
+    df_scalar = []
+
+    for cols in features.keys():
+        z_prod = (features[cols]-scaler[cols][0])/scaler[cols][1]
+        df_scalar.append(z_prod)
+
+    df_scalar = pd.DataFrame([df_scalar], columns=labels)
+    prediction = model.predict(df_scalar)[0]
+
+    # result = model.predict(np.array([rooms,
+    #     distance,
+    #     bathroom,
+    #     car,
+    #     landsize,
+    #     lattitude,
+    #     longtitude,
+    #     propertycount, 
+    #     month]).reshape(1,9))[0]
+    
+    return render_template('index.html', result=prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
